f0fb34f0b56aea91995dc5eead5d43310b5c33a9
Shriyansh kumar <shriyansh7870@gmail.com>
2022-06-11 14:41:40 +0530

    Array

---
+++ Letsjavacoding/Max_minium.java
@@ -0,0 +1,42 @@
+package Letsjavacoding;
+
+public class Max_minium {
+    static class Pair{
+        int min;
+        int max;
+    }
+    static Pair getminmax (int arr[],int n){
+        Pair minmax = new Pair();
+        int i;
+        if (n == 1) {
+            minmax.max = arr[0];
+            minmax.min = arr[0];
+            return minmax;
+        }
+        if (arr[0] > arr[1]) {
+            minmax.max = arr[0];
+            minmax.min = arr[1];
+        } else {
+            minmax.max = arr[1];
+            minmax.min = arr[0];
+        }
+
+        for (i = 2; i < n; i++) {
+            if (arr[i] > minmax.max) {
+                minmax.max = arr[i];
+            } else if (arr[i] < minmax.min) {
+                minmax.min = arr[i];
+            }
+        }
+
+        return minmax;
+    }
+    public static void main(String[]args){
+        int arr[]={1000,11,45,1,3000,450};
+        int arr_size=6;
+        Pair minmax = getminmax(arr, arr_size);
+        System.out.printf("\nMinimum element is %d", minmax.min);
+        System.out.printf("\nMaximum element is %d", minmax.max);
+
+    }
+}
